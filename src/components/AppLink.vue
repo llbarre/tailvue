d2522afbcf3b05c88d26a06c65ecf07f21a713e8
Luiz Barreto <llbarre@hotmail.com>
2023-11-25 19:00:22 -0600

    feat: add AppLink

---
+++ src/components/AppLink.vue
@@ -0,0 +1,25 @@
+<template>
+    <component :is="componentType" :to="isHref ? undefined : to" :href="isHref ? props.to : undefined" v-bind="$attrs">
+        <slot />
+    </component>
+</template>
+  
+<script setup>
+import { computed } from 'vue';
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+    ...RouterLink.props,
+    to: {
+        type: String,
+        default: "",
+    },
+});
+
+const isHref = computed(() => props.to && typeof props.to === "string" && props.to.startsWith("http"));
+
+// Determine the component type based on 'isHref'
+const componentType = computed(() => isHref.value ? 'a' : RouterLink);
+
+</script>
+  
