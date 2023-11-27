6e01110e77f8cccfa85d12b33bc5cef82f67967c
Luiz Barreto <llbarre@hotmail.com>
2023-11-27 11:27:01 -0600

    fix: missing auto import for Vue Router

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import VueRouter from "unplugin-vue-router/vite";
+import { VueRouterAutoImports } from "unplugin-vue-router"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +18,7 @@ export default defineConfig({
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
-      imports: ["vue", "vue-router", "@vueuse/core"],
+      imports: ["vue", VueRouterAutoImports, "@vueuse/core"],
       dts: false,
       dirs: ["./src/composables"],
       vueTemplate: true,
